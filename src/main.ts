2585a0cbaccd086467b1f600e196fe43c2050b1f
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2023-08-06 13:31:35 +0300

    cleaning

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import "@/style/normalize.css";
+import "@/style/style.css";
+import "@/style/fonts.css";
+
 createApp(App).use(router).mount("#app");
